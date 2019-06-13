e9592f7c3df138a53feeba99feb9996e54e1b1f5
Raquib-ul Alam <rala7323@uni.sydney.edu.au>
2019-06-13 15:35:36 +1000

    Calculate max mep size in all frames of MEP

--- 05-mep-class.py
+++ 05-mep-class.py
@@ -16,46 +16,94 @@ mep_present = len(meps) > 0
 eegs = sorted(glob.glob(experiment + '/eeg/*/*.cnt'))
 eeg_present = len(eegs) > 0
 all_present = cmap_present and mep_present and eeg_present
-if all_present:
-    for mep in meps:
-        #  Plot MEP
-        fileMep = open(mep, "r+")
-        mepFrames = fileMep.read().split('\n\n')
-        for num in range(10):
-            # Read a MEP frame.
-            df = pd.read_csv(StringIO(mepFrames[num]), '\t')
-            if 'L APB' in df:
-                apb_name = 'L APB'
-            else:
-                apb_name = 'APB'
+print(all_present)
 
-            # Crop region of interest and delete stimulation artifact.
-            crop_start = 0.211
-            crop_end = 0.4
-            multiplier = 7499 / 1.4998
-            start = int(crop_start * multiplier)
-            end = int(crop_end * multiplier)
-            mep_cropped = df.iloc[start:end, :]
-            time = df.iloc[start:end, :]
+#%% Function to open MEP file.
+def open_mep_as_df(path):
+    fileMep = open(path, "r+")
+    mep_frames = fileMep.read().split('\n\n')
+    df_list = []
+    for mep_frame in mep_frames:
+        df_list.append(pd.read_csv(StringIO(mep_frame), '\t'))
+    return df_list
 
-            # Calculate peaks.
-            max_row = df.iloc[mep_cropped.idxmax(axis=0)[apb_name]]
-            min_row = df.iloc[mep_cropped.idxmin(axis=0)[apb_name]]
+#%% Function to crop MEP region.
+def crop_mep_region(mep_frame):
+    crop_start = 0.211
+    crop_end = 0.4
+    multiplier = 7499 / 1.4998
+    start = int(crop_start * multiplier)
+    end = int(crop_end * multiplier)
+    mep_cropped = mep_frame.iloc[start:end, :]
+    time = mep_frame.iloc[start:end, :]
+    return mep_cropped, time
 
-            # Plot the MEP.
-            plt.figure(figsize=(14, 5))
-            fig, axs = plt.subplots(1, 1, figsize=(14, 5))
-            axs.plot(df['s'], df[apb_name])
-            axs.set_title(apb_name + ' Frame ' + str(num))
-            plt.axvspan(crop_start, crop_end, color='red', alpha=0.1)
-            plt.suptitle(mep)
-            
-            # Indicate peaks.
-            axs.plot(max_row.s, max_row[apb_name], 'ro')
-            axs.plot(min_row.s, min_row[apb_name], 'ro')
-            axs.annotate(str(max_row[apb_name]), (max_row.s, max_row[apb_name]))
-            axs.annotate(str(min_row[apb_name]), (min_row.s, min_row[apb_name]))
-            plt.show()
-        break
+#%% Function to get APB column name.
+def get_apb_column_name(mep_frame):
+    if 'L APB' in df:
+        return 'L APB'
+    else:
+        return 'APB'
+
+#%% Calculate the max and min peaks across all frames/epochs.
+max_meps_among_all_frames_list = []
+for mep in meps:
+    mep_frames = open_mep_as_df(mep)
+    max_in_frame_list = []
+    for frame in range(len(mep_frames)):
+        df = mep_frames[frame]
+        mep_cropped, time = crop_mep_region(df)
+        max_row = df.iloc[mep_cropped.idxmax(axis=0)[apb_name]]
+        min_row = df.iloc[mep_cropped.idxmin(axis=0)[apb_name]]
+        apb_name = get_apb_column_name(df)
+        max_in_frame_list.append({
+            'path': mep,
+            'frame': frame,
+            's-max': max_row['s'], 
+            'apb-max': max_row[apb_name], 
+            's-min': min_row['s'], 
+            'apb-min': min_row[apb_name],
+            'mep-size': max_row[apb_name] - min_row[apb_name]
+            })
+    df = pd.DataFrame(max_in_frame_list)
+    df = df.sort_values(by=['mep-size'], ascending=False)
+    max_meps_among_all_frames_list.append(df.iloc[0])
+max_meps_among_all_frames_df = pd.DataFrame(max_meps_among_all_frames_list)
+max_meps_among_all_frames_df = max_meps_among_all_frames_df.set_index('path')
+
+#%% Identify peaks and plot them.
+for mep in meps:
+    #  Plot MEP
+    df_list = open_mep_as_df(mep)
+    for num in range(10):
+        # Read a MEP frame.
+        df = df_list[num]
+        apb_name = get_apb_column_name(df)
+
+        # Crop region of interest and delete stimulation artifact.
+        mep_cropped, time = crop_mep_region(df)
+
+        # Calculate peaks.
+        max_row = df.iloc[mep_cropped.idxmax(axis=0)[apb_name]]
+        min_row = df.iloc[mep_cropped.idxmin(axis=0)[apb_name]]
+
+        # Plot the MEP.
+        plt.figure(figsize=(14, 5))
+        fig, axs = plt.subplots(1, 1, figsize=(14, 5))
+        axs.plot(df['s'], df[apb_name])
+        axs.set_title(apb_name + ' Frame ' + str(num))
+        plt.axvspan(crop_start, crop_end, color='red', alpha=0.1)
+        plt.suptitle(mep)
+        
+        # Indicate peaks.
+        axs.plot(max_row.s, max_row[apb_name], 'ro')
+        axs.plot(min_row.s, min_row[apb_name], 'ro')
+        axs.annotate(str(max_row[apb_name]), (max_row.s, max_row[apb_name]))
+        axs.annotate(str(min_row[apb_name]), (min_row.s, min_row[apb_name]))
+        plt.show()
+
+        # Compare with max size.
+    break
 
 #%%
+# 
