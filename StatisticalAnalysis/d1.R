aff6fcaa583a6b4326f9f9130bbbe4c6457ef01b
Raquib-ul Alam <alam.kanak@gmail.com>
2020-09-25 11:22:04 +0600

    Run LMM

--- StatisticalAnalysis/d1.R
+++ StatisticalAnalysis/d1.R
@@ -22,51 +22,20 @@ library(ggplot2)
 library(multcomp) # for glht
 library(emmeans) # for emm
 
-# Prepare ggplot theme ----------------------------------------------------------
-theme_ms <- function(base_size=12, base_family="Helvetica") {
-  library(grid)
-  (theme_bw(base_size = base_size, base_family = base_family)+
-      theme(text=element_text(color="black"),
-            axis.title=element_text(face="bold", size = rel(1.3)),
-            axis.text=element_text(size = rel(1), color = "black"),
-            legend.title=element_text(face="bold"),
-            legend.text=element_text(face="bold"),
-            legend.background=element_rect(fill="transparent"),
-            legend.key.size = unit(0.8, 'lines'),
-            panel.border=element_rect(color="black",size=1),
-            panel.grid=element_blank()
-      ))
-}
-
-
-# Prepare power dataset ---------------------------------------------------------
+# Prepare power and phase dataset ---------------------------------------------------------
 
 # Load powers
-df_power = read_excel("153-d1-power-long.xlsx")
-df_power$Resampled = TRUE
-df_power$ArtifactRemoved = TRUE
-df = read_excel("158-d1-power-long.xlsx")
-df$Resampled = FALSE
-df$ArtifactRemoved = TRUE
-df_power = bind_rows(df_power, df)
-df = read_excel("159-d1-power-long.xlsx")
-df$Resampled = TRUE
-df$ArtifactRemoved = FALSE
-df_power = bind_rows(df_power, df)
-df = read_excel("161-d1-power-long.xlsx")
-df$Resampled = FALSE
-df$ArtifactRemoved = FALSE
-df_power = bind_rows(df_power, df)
-df_power$Filter[df_power$Filter == "Blackman-Harris"] <- "Blackmann-Harris"
+df_power = read.csv("../164-d1-powers.csv")
+df_phase = read.csv("../164-d1-phases.csv")
 
 # Fix column ordering
 df_power <- transform(df_power, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
 df_power <- transform(df_power, Method=factor(Method,levels=c("FFT","Welch","Burg")))
-df_power <- transform(df_power, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
+df_power <- transform(df_power, EEG=factor(EEG,levels=c("Raw","Hjorth","Averaged")))
 
 # Transform some columns
 df_power$sub = factor(df_power$sub)
-df_power$trial = factor(df_power$trial_abs)
+df_power$trial_abs = factor(df_power$trial_abs)
 df_power$Method = factor(df_power$Method)
 df_power$Band = factor(df_power$Band)
 df_power$Filter = factor(df_power$Filter)
@@ -76,8 +45,8 @@ df_power$Resampled = factor(df_power$Resampled)
 df_power$ArtifactRemoved = factor(df_power$ArtifactRemoved)
 
 # For post-hoc pairwise comparison
-contrasts(df_power$sub) <- "contr.sum"
-contrasts(df_power$trial) <- "contr.sum"
+# contrasts(df_power$sub) <- "contr.sum"
+contrasts(df_power$trial_abs) <- "contr.sum"
 contrasts(df_power$Method) <- "contr.sum"
 contrasts(df_power$Band) <- "contr.sum"
 contrasts(df_power$Filter) <- "contr.sum"
@@ -86,244 +55,39 @@ contrasts(df_power$EEG) <- "contr.sum"
 contrasts(df_power$Resampled) <- "contr.sum"
 contrasts(df_power$ArtifactRemoved) <- "contr.sum"
 
-
-# TMS-EEG - LMM - Power ~ variables ---------------------------------
-
-# Default values
-def_variables = c('ArtifactRemoved', 'EEG', 'Resampled', 'Filter', 'Time', 'Method')
-def_values = c(TRUE, 'Raw', FALSE, 'Butterworth', -750, 'Welch')
-
-# Power ~ Factor + (1|sub) + (1|band)
-result <- data.frame(
-  modelNo=rep("",3), 
-  obsCount=rep("", 3),
-  factor=rep("", 3),
-  p=rep("", 3), 
-  f=rep("", 3), 
-  dfn=rep("", 3), 
-  dfd=rep("", 3), 
-  stringsAsFactors=FALSE)
-modelNo <- 1
-for (var in def_variables) {
-  df2 = df_power
-  j = 1
-  for (var2 in def_variables) {
-    if (var != var2) {
-      df2 <- subset(df2, get(var2)==def_values[j])
-    }
-    j = j + 1
-  }
-  m = lmer(value ~ get(var)  + (1|sub/Band), data=df2)
-  m = anova(m)
-  result[modelNo, ] <- list(modelNo, nrow(df2), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-  modelNo <- modelNo + 1
-}
-
-# TMS-EEG - LMM - Power ~ interaction ---------------------------------
-
-# Power ~ Factor + (1|sub) + (1|band)
-result <- data.frame(
-  modelNo=rep("",3), 
-  obsCount=rep("", 3),
-  factor=rep("", 3),
-  p=rep("", 3), 
-  f=rep("", 3), 
-  dfn=rep("", 3), 
-  dfd=rep("", 3), 
-  stringsAsFactors=FALSE)
-modelNo <- 1
-combs <- combn(def_variables, 2)
-for (var in 1:ncol(combs)) {
-  var = combs[,var]
-  df2 = df_power
-  j = 1
-  for (var2 in def_variables) {
-    if (var[1] != var2 && var[2] != var2) {
-      df2 <- subset(df2, get(var2)==def_values[j])
-    }
-    j = j + 1
-  }
-  m = lmer(value ~ (get(var[1]) * get(var[2])) + (1|sub/Band), data=df2)
-  m = anova(m)
-  result[modelNo, ] <- list(modelNo, nrow(df2), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-  modelNo <- modelNo + 1
-}
-
-# Prepare phase dataset ---------------------------------------------------
+# Convert to log
+df_power$mep_size_log = log(df_power$mep_size)
+df_power$mep_latency_log = log(df_power$mep_latency)
+df_power$mep_duration_log = log(df_power$mep_duration)
+df_power$mep_area_log = log(df_power$mep_area)
 
 # Load phases
-df_phase = read_excel("153-d1-phase-long.xlsx")
-df_phase$Resampled = TRUE
-df_phase$ArtifactRemoved = TRUE
-df = read_excel("158-d1-phase-long.xlsx")
-df$Resampled = FALSE
-df$ArtifactRemoved = TRUE
-df_phase = bind_rows(df_phase, df)
-df = read_excel("159-d1-phase-long.xlsx")
-df$Resampled = TRUE
-df$ArtifactRemoved = FALSE
-df_phase = bind_rows(df_phase, df)
-df = read_excel("161-d1-phase-long.xlsx")
-df$Resampled = FALSE
-df$ArtifactRemoved = FALSE
-df_phase = bind_rows(df_phase, df)
-df_phase$Filter[df_phase$Filter == "Blackman-Harris"] <- "Blackmann-Harris"
-
 # Fix column ordering
 df_phase <- transform(df_phase, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
-df_phase <- transform(df_phase, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
+df_phase <- transform(df_phase, EEG=factor(EEG,levels=c("Raw","Hjorth","Averaged")))
 
 # Transform some columns
 df_phase$sub = factor(df_phase$sub)
-df_phase$trial = factor(df_phase$trial_abs)
+df_phase$trial_abs = factor(df_phase$trial_abs)
 df_phase$Band = factor(df_phase$Band)
 df_phase$Filter = factor(df_phase$Filter)
 df_phase$EEG = factor(df_phase$EEG)
 df_phase$ArtifactRemoved = factor(df_phase$ArtifactRemoved)
 
 # For post-hoc pairwise comparison
-contrasts(df_phase$sub) <- "contr.sum"
-contrasts(df_phase$trial) <- "contr.sum"
+# contrasts(df_phase$sub) <- "contr.sum"
+contrasts(df_phase$trial_abs) <- "contr.sum"
 contrasts(df_phase$Band) <- "contr.sum"
 contrasts(df_phase$Filter) <- "contr.sum"
 contrasts(df_phase$EEG) <- "contr.sum"
 contrasts(df_phase$ArtifactRemoved) <- "contr.sum"
 
 
-
-# TMS-EEG - LMM - Phase - Variables ---------------------------------
-
-# Default values
-def_variables = c('ArtifactRemoved', 'EEG', 'Filter')
-def_values = c(TRUE, 'Raw', 'Butterworth')
-
-# Phase ~ Factor + (1|sub) + (1|band)
-result <- data.frame(
-  modelNo=rep("",3), 
-  phase=rep("", 3),
-  obsCount=rep("", 3),
-  factor=rep("", 3),
-  p=rep("", 3), 
-  f=rep("", 3), 
-  dfn=rep("", 3), 
-  dfd=rep("", 3), 
-  stringsAsFactors=FALSE)
-modelNo <- 1
-for (phase_type in c('peak', 'trough')) {
-  if (phase_type == 'peak') {
-    df2 = subset(df_phase, value <= 180)
-  }
-  else {
-    df2 = subset(df_phase, value > 180)
-  }
-  for (var in def_variables) {
-    df3 = df2
-    j = 1
-    for (var2 in def_variables) {
-      if (var != var2) {
-        df3 <- subset(df3, get(var2)==def_values[j])
-      }
-      j = j + 1
-    }
-    m = lmer(value ~ get(var)  + (1|sub/Band), data=df3)
-    m = anova(m)
-    result[modelNo, ] <- list(modelNo, phase_type, nrow(df3), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-    modelNo <- modelNo + 1
-  }
-}
-
-# Linear mixed effect model for interactions ---------------------------------
-
-# Phase ~ Factor + (1|sub) + (1|band)
-result <- data.frame(
-  modelNo=rep("",3), 
-  phase=rep("",3),
-  obsCount=rep("", 3),
-  factor=rep("", 3),
-  p=rep("", 3), 
-  f=rep("", 3), 
-  dfn=rep("", 3), 
-  dfd=rep("", 3), 
-  stringsAsFactors=FALSE)
-modelNo <- 1
-combs <- combn(def_variables, 2)
-for (phase_type in c('peak', 'trough')) {
-  if (phase_type == 'peak') {
-    df2 = subset(df_phase, value <= 180)
-  }
-  else {
-    df2 = subset(df_phase, value > 180)
-  }
-  for (var in 1:ncol(combs)) {
-    var = combs[,var]
-    df3 = df2
-    j = 1
-    for (var2 in def_variables) {
-      if (var[1] != var2 && var[2] != var2) {
-        print(paste(var2, '=', def_values[j], 'for constant', var[1], 'and', var[2]))
-        df3 <- subset(df3, get(var2)==def_values[j])
-      }
-      j = j + 1
-    }
-    m = lmer(value ~ (get(var[1]) * get(var[2]))  + (1|sub/Band), data=df3)
-    m = anova(m)
-    result[modelNo, ] <- list(modelNo, phase_type, nrow(df3), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-    modelNo <- modelNo + 1
-  }
-}
-
-
-# Load alcohol powers and phases ------------------------------------------
-
-# Load powers
-df_power = read_excel("157-alc-power-long.xlsx")
-df_phase = read_excel("157-alc-phase-long.xlsx")
-df_power = subset(df_power, alcholic==FALSE)
-df_phase = subset(df_phase, alcholic==FALSE)
-
-# Column ordering
-df_power <- transform(df_power, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
-df_power <- transform(df_power, Method=factor(Method,levels=c("FFT","Welch","Burg")))
-df_power <- transform(df_power, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
-df_phase <- transform(df_phase, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
-df_phase <- transform(df_phase, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
-
-
-# Transform some columns
-df_power$sub = factor(df_power$sub)
-df_power$trial = factor(df_power$trial)
-df_power$Method = factor(df_power$Method)
-df_power$Band = factor(df_power$Band)
-df_power$Filter = factor(df_power$Filter)
-df_power$Time = factor(df_power$Time)
-df_power$EEG = factor(df_power$EEG)
-
-df_phase$sub = factor(df_phase$sub)
-df_phase$trial = factor(df_phase$trial)
-df_phase$Band = factor(df_phase$Band)
-df_phase$Filter = factor(df_phase$Filter)
-df_phase$EEG = factor(df_phase$EEG)
-
-# For post-hoc pairwise comparison
-contrasts(df_power$sub) <- "contr.sum"
-contrasts(df_power$trial) <- "contr.sum"
-contrasts(df_power$Method) <- "contr.sum"
-contrasts(df_power$Band) <- "contr.sum"
-contrasts(df_power$Filter) <- "contr.sum"
-contrasts(df_power$Time) <- "contr.sum"
-contrasts(df_power$EEG) <- "contr.sum"
-
-contrasts(df_phase$sub) <- "contr.sum"
-contrasts(df_phase$trial) <- "contr.sum"
-contrasts(df_phase$Band) <- "contr.sum"
-contrasts(df_phase$Filter) <- "contr.sum"
-contrasts(df_phase$EEG) <- "contr.sum"
-
-# UCI - Power - Linear mixed model ------------------------------------------------
+# LMM: Power ~ factors ---------------------------------------------------------
 
 # Default values
-def_variables = c('EEG', 'Filter', 'Time', 'Method')
-def_values = c('Raw', 'Butterworth', -750, 'Welch')
+power_def_variables = c('ArtifactRemoved', 'EEG', 'Resampled', 'Filter', 'Time', 'Method')
+power_def_values = c('True', 'Raw', 'False', 'Butterworth', -750, 'Welch')
 
 # Power ~ Factor + (1|sub) + (1|band)
 result <- data.frame(
@@ -336,23 +100,24 @@ result <- data.frame(
   dfd=rep("", 3), 
   stringsAsFactors=FALSE)
 modelNo <- 1
-for (var in def_variables) {
+for (var in power_def_variables) {
   df2 = df_power
   j = 1
-  for (var2 in def_variables) {
+  for (var2 in power_def_variables) {
     if (var != var2) {
-      df2 <- subset(df2, get(var2)==def_values[j])
+      df2 <- subset(df2, get(var2)==power_def_values[j])
+      print(paste(var2, '=', power_def_values[j], ', nrow =', nrow(df2)))
     }
     j = j + 1
   }
-  m = lmer(value ~ get(var)  + (1|sub) + (1|Band), data=df2)
+  m = lmer(Power ~ get(var)  + (1|sub/Band), data=df2)
   m = anova(m)
   result[modelNo, ] <- list(modelNo, nrow(df2), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
   modelNo <- modelNo + 1
 }
 
 
-# UCI LMM - Power - Interaction ---------------------------------
+# LMM: Power ~ Interaction -----------------------------------------------------
 
 result <- data.frame(
   modelNo=rep("",3), 
@@ -364,30 +129,30 @@ result <- data.frame(
   dfd=rep("", 3), 
   stringsAsFactors=FALSE)
 modelNo <- 1
-combs <- combn(def_variables, 2)
+combs <- combn(power_def_variables, 2)
 for (var in 1:ncol(combs)) {
   var = combs[,var]
   df2 = df_power
   j = 1
-  for (var2 in def_variables) {
+  for (var2 in power_def_variables) {
     if (var[1] != var2 && var[2] != var2) {
-      df2 <- subset(df2, get(var2)==def_values[j])
+      df2 <- subset(df2, get(var2)==power_def_values[j])
+      print(paste(var2, '=', power_def_values[j], ', nrow =', nrow(df2)))
     }
     j = j + 1
   }
-  m = lmer(value ~ (get(var[1]) * get(var[2])) + (1|sub) + (1|Band), data=df2)
+  m = lmer(Power ~ (get(var[1]) * get(var[2])) + (1|sub/Band), data=df2)
   m = anova(m)
   result[modelNo, ] <- list(modelNo, nrow(df2), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
   modelNo <- modelNo + 1
 }
 
 
-
-# UCI LMM - Phase - Variables ---------------------------------
+# LMM: Phase ~ Factors ----------------------------------------------------
 
 # Default values
-def_variables = c('EEG', 'Filter')
-def_values = c('Raw', 'Butterworth')
+phase_def_variables = c('ArtifactRemoved', 'EEG', 'Filter')
+phase_def_values = c('True', 'Raw', 'Butterworth')
 
 # Phase ~ Factor + (1|sub) + (1|band)
 result <- data.frame(
@@ -403,30 +168,32 @@ result <- data.frame(
 modelNo <- 1
 for (phase_type in c('peak', 'trough')) {
   if (phase_type == 'peak') {
-    df2 = subset(df_phase, value <= 180)
+    df2 = subset(df_phase, Phase <= 180)
   }
   else {
-    df2 = subset(df_phase, value > 180)
+    df2 = subset(df_phase, Phase > 180)
   }
-  for (var in def_variables) {
+  for (var in phase_def_variables) {
     df3 = df2
     j = 1
-    for (var2 in def_variables) {
+    for (var2 in phase_def_variables) {
       if (var != var2) {
-        df3 <- subset(df3, get(var2)==def_values[j])
+        df3 <- subset(df3, get(var2)==phase_def_values[j])
+        print(paste(var2, '=', phase_def_values[j], ', nrow =', nrow(df3)))
       }
       j = j + 1
     }
-    m = lmer(value ~ get(var)  + (1|sub) + (1|Band), data=df3)
+    m = lmer(Phase ~ get(var)  + (1|sub/Band), data=df3)
     m = anova(m)
     result[modelNo, ] <- list(modelNo, phase_type, nrow(df3), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
     modelNo <- modelNo + 1
   }
 }
 
-# UCI LMM - Phase - Interaction ---------------------------------
 
-# Phase ~ (Factor1 * Factor2) + (1|sub) + (1|band)
+# LMM: Phase ~ Interaction ------------------------------------------------
+
+# Phase ~ Factor + (1|sub) + (1|band)
 result <- data.frame(
   modelNo=rep("",3), 
   phase=rep("",3),
@@ -438,71 +205,76 @@ result <- data.frame(
   dfd=rep("", 3), 
   stringsAsFactors=FALSE)
 modelNo <- 1
-combs <- combn(def_variables, 2)
+combs <- combn(phase_def_variables, 2)
 for (phase_type in c('peak', 'trough')) {
   if (phase_type == 'peak') {
-    df2 = subset(df_phase, value <= 180)
+    df2 = subset(df_phase, Phase <= 180)
   }
   else {
-    df2 = subset(df_phase, value > 180)
+    df2 = subset(df_phase, Phase > 180)
   }
   for (var in 1:ncol(combs)) {
     var = combs[,var]
     df3 = df2
     j = 1
-    for (var2 in def_variables) {
+    for (var2 in phase_def_variables) {
       if (var[1] != var2 && var[2] != var2) {
-        print(paste(var2, '=', def_values[j], 'for constant', var[1], 'and', var[2]))
-        df3 <- subset(df3, get(var2)==def_values[j])
+        df3 <- subset(df3, get(var2)==phase_def_values[j])
+        print(paste(var2, '=', phase_def_values[j], ', nrow=', nrow(df3)))
       }
       j = j + 1
     }
-    m = lmer(value ~ (get(var[1]) * get(var[2]))  + (1|sub) + (1|Band), data=df3)
+    m = lmer(Phase ~ (get(var[1]) * get(var[2]))  + (1|sub/Band), data=df3)
     m = anova(m)
     result[modelNo, ] <- list(modelNo, phase_type, nrow(df3), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
     modelNo <- modelNo + 1
   }
 }
 
+# Combine powers and phases for MEP LMM -----------------------------------------------
 
+df_mep = subset(df_power, Resampled=='True')
+df_mep = subset(df_mep, ArtifactRemoved=='True')
+df_mep = subset(df_mep, Method=='Welch')
+df_mep = subset(df_mep, Filter=='Butterworth')
+df_mep = subset(df_mep, EEG=='Averaged')
+df_mep = subset(df_mep, Time=='-150')
 
-# Test --------------------------------------------------------------------
-
-# var = 'Filter'
-# sub = 'sub22'
-# df2 = df_power
-# j = 1
-# for (var2 in def_variables) {
-#   if (var != var2) {
-#     df2 <- subset(df2, get(var2)==def_values[j])
-#   }
-#   j = j + 1
-# }
-unique(df_power$sub)
-for (sub2 in unique(df_power$sub)) {
-  df2 <- subset(df_power, sub=='sub21')
-  df2 <- subset(df2, ArtifactRemoved==FALSE)
-  df2 <- subset(df2, EEG=='Raw')
-  df2 <- subset(df2, Resampled==FALSE)
-  df2 <- subset(df2, Time==-750)
-  df2 <- subset(df2, Method=='Welch')
-  df2 <- subset(df2, Band=='Mu')
-  labels <- seq(0, 150, by=10)
-  g = ggplot(df2, aes(x=trial, y=value, group=Filter, color=Filter)) +
-    geom_line() +
-    scale_x_discrete(breaks=labels, labels=as.character(labels)) +
-    ylab('Log power') +
-    xlab('Trial') +
-    ggtitle('Effect of filters on power trials') +
-    theme_ms()
-  print(g)
+df_mep$Phase <- 0
+pb = txtProgressBar(min = 0, max = nrow(df_mep), initial = 0, style=3)
+for (i in 1:nrow(df_mep)) {
+  df_test = subset(df_phase, ArtifactRemoved==df_mep$ArtifactRemoved[i])
+  df_test = subset(df_test, Filter==df_mep$Filter[i])
+  df_test = subset(df_test, EEG==df_mep$EEG[i])
+  df_test = subset(df_test, Band==df_mep$Band[i])
+  df_test = subset(df_test, sub==df_mep$sub[i])
+  df_test = subset(df_test, trial_abs==df_mep$trial_abs[i])
+  df_mep$Phase[i] <- df_test$Phase
+  if (nrow(df_test) != 1) {
+    print(paste('Error in ', i))
+    break
+  }
+  setTxtProgressBar(pb, i)
 }
+close(pb)
 
+# LMM: MEP ~ Power * Phase  ---------------------------------
 
-ggplot(df2, aes(x=Band, y=value, fill=Filter)) + 
-  geom_boxplot() + 
-  scale_color_grey() + 
-  theme_ms() + 
-  theme(strip.text.x = element_blank()) +
-  ylab("Power (dB)")
+result <- data.frame(target=rep("", 5), band=rep("", 5), input=rep("", 5), p=rep(0, 5), b=rep(0, 5), no_of_obs=rep(0, 5), stringsAsFactors=FALSE)
+i <- 1
+for (variable in c("mep_size_log", "mep_latency_log", "mep_duration_log", 'mep_area_log')) {
+  for (band in c('Theta', 'Mu', 'Beta', 'Gamma')) {
+    df_mep2 = subset(df_mep, Band==band)
+    df_mep2 <- subset(df_mep2, (Phase > 45 & Phase < 135) | (Phase > 225 & Phase < 315))
+    df_mep2$Phase_bin <- factor(cut(df_mep2$Phase, breaks=c(0,180,360), labels=c(1, 0)))
 
+    formula <- paste(variable, ' ~ (Power * Phase_bin) + (1|sub)', sep='')
+    m = lmer(as.formula(formula), data=df_mep2)
+    m = summary(m)
+
+    result[i, ] <- list(variable, band, 'power', round(m$coefficients[2, 5], 4), round(m$coefficients[2, 1], 4), nrow(df_power2))
+    result[i+1, ] <- list(variable, band, 'phase', round(m$coefficients[3, 5], 4), round(m$coefficients[3, 1], 4), nrow(df_power2))
+    result[i+2, ] <- list(variable, band, 'interaction', round(m$coefficients[4, 5], 4), round(m$coefficients[4, 1], 4), nrow(df_power2))
+    i=i+3
+  }
+}
--- StatisticalAnalysis/d3.R
+++ StatisticalAnalysis/d3.R
@@ -26,26 +26,22 @@ library(emmeans) # for emm
 # Prepare dataset ---------------------------------------------------------
 
 # Load file 
-df_power = read.csv("../157-d2-power-v2.csv")
-df_power = subset(df_power, alcoholic=='False')
-df_power = subset(df_power, condition=='S1 obj')
+df_power = read.csv("../166-d3-powers.csv")
 df_power = df_power[is.finite(df_power$Power),]
-df_phase = read.csv("../157-d2-phase-v2.csv")
-df_phase = subset(df_phase, alcoholic=='False')
-df_phase = subset(df_phase, condition=='S1 obj')
+df_phase = read.csv("../166-d3-phases.csv")
 
 # Fix column ordering
 df_power <- transform(df_power, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
 df_power <- transform(df_power, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
 df_power <- transform(df_power, Method=factor(Method,levels=c("FFT","Welch","Burg")))
-df_power <- transform(df_power, ArtifactRemoved=factor(ArtifactRemoved,levels=c('True', 'False')))
+df_power <- transform(df_power, ArtifactRemoved=factor(ArtifactRemoved,levels=c("True","False")))
 df_phase <- transform(df_phase, Band=factor(Band,levels=c("Theta","Mu","Beta","Gamma")))
 df_phase <- transform(df_phase, EEG=factor(EEG,levels=c("Raw","Hjorth","Average")))
-df_phase <- transform(df_phase, ArtifactRemoved=factor(ArtifactRemoved,levels=c('True', 'False')))
+df_phase <- transform(df_phase, ArtifactRemoved=factor(ArtifactRemoved,levels=c("True","False")))
 
 # Transform some columns
 df_power$sub = factor(df_power$sub)
-df_power$trial_abs = factor(df_power$trial_abs)
+df_power$trial_abs = factor(df_power$trial)
 df_power$Method = factor(df_power$Method)
 df_power$Band = factor(df_power$Band)
 df_power$Filter = factor(df_power$Filter)
@@ -54,7 +50,7 @@ df_power$EEG = factor(df_power$EEG)
 df_power$ArtifactRemoved = factor(df_power$ArtifactRemoved)
 
 df_phase$sub = factor(df_phase$sub)
-df_phase$trial_abs = factor(df_phase$trial_abs)
+df_phase$trial_abs = factor(df_phase$trial)
 df_phase$Band = factor(df_phase$Band)
 df_phase$Filter = factor(df_phase$Filter)
 df_phase$EEG = factor(df_phase$EEG)
@@ -85,7 +81,7 @@ power_def_values = c('Raw', 'Butterworth', -750, 'Welch', 'True')
 
 # Power ~ Factor + (1|sub) + (1|band)
 result <- data.frame(
-  Band=rep("", 3),
+  band=rep("",3),
   modelNo=rep("",3), 
   obsCount=rep("", 3),
   factor=rep("", 3),
@@ -96,9 +92,8 @@ result <- data.frame(
   stringsAsFactors=FALSE)
 modelNo <- 1
 for (var in power_def_variables) {
-  for (band in c('Theta', 'Mu', 'Beta', 'Gamma')) {
-    df2 = df_power
-    df2 <- subset(df2, Band==band)
+  for (band in c("Theta", "Mu", "Beta", "Gamma")) {
+    df2 <- subset(df_power, Band==band)
     j = 1
     for (var2 in power_def_variables) {
       if (var != var2) {
@@ -109,12 +104,11 @@ for (var in power_def_variables) {
     }
     m = lmer(Power ~ get(var)  + (1|sub), data=df2)
     m = anova(m)
-    # result[modelNo, ] <- list(modelNo, nrow(df2), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-    # result[modelNo, ] <- list(modelNo, nrow(df2), var, round(m[3, 6], 4), round(m[3, 5], 2), round(m[3, 3], 2), round(m[3, 4], 2))
     result[modelNo, ] <- list(band, modelNo, nrow(df2), var, round(m[1, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
     modelNo <- modelNo + 1
   }
 }
+
 
 # LMM: Power ~ Interaction -----------------------------------------------------
 
@@ -140,10 +134,9 @@ for (var in 1:ncol(combs)) {
     }
     j = j + 1
   }
-  m = lmer(Power ~ (get(var[1]) * get(var[2]) * Band) + (1|sub), data=df2)
+  m = lmer(Power ~ (get(var[1]) * get(var[2])) + (1|sub/Band), data=df2)
   m = anova(m)
-  # result[modelNo, ] <- list(modelNo, nrow(df2), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
-  result[modelNo, ] <- list(modelNo, nrow(df2), paste(var[1], var[2], sep=":"), round(m[7, 6], 4), round(m[7, 5], 2), round(m[7, 3], 2), round(m[7, 4], 2))
+  result[modelNo, ] <- list(modelNo, nrow(df2), paste(var[1], var[2], sep=":"), round(m[3, 6], 4), round(m[1, 5], 2), round(m[1, 3], 2), round(m[1, 4], 2))
   modelNo <- modelNo + 1
 }
 
@@ -175,8 +168,9 @@ for (phase_type in c('peak', 'trough')) {
     df2 = subset(df_phase, Phase > 180)
   }
   for (var in phase_def_variables) {
+    
     for (band in c("Theta", "Mu", "Beta", "Gamma")) {
-      df3 <- subset(df2, Band == band)
+      df3 <- subset(df2, Band==band)
       j = 1
       for (var2 in phase_def_variables) {
         if (var != var2) {
@@ -233,22 +227,3 @@ for (phase_type in c('peak', 'trough')) {
     modelNo <- modelNo + 1
   }
 }
-
-
-# ANOVA -------------------------------------------------------------------
-
-var = 'ArtifactRemoved'
-df2 <- df_power
-j = 1
-for (var2 in power_def_variables) {
-  if (var != var2) {
-    df2 <- subset(df2, get(var2)==power_def_values[j])
-    print(paste(var2, '=', power_def_values[j], ', nrow =', nrow(df2)))
-  }
-  j = j + 1
-}
-
-df2 %>% group_by(trial_abs) %>% identify_outliers(Power)
-df2 %>% group_by(trial_abs) %>% shapiro_test(Power)
-res.aov <- anova_test(data=df2, dv=Power, wid=sub, within=ArtifactRemoved)
-get_anova_table(res.aov)
